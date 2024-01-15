93ff13ab13adc4c73ae53cc5e3571780dac69638
Diego Parula <parula27@gmail.com>
2024-01-15 17:40:38 -0300

    creacion de proyecto

---
+++ proyecto-pizzeria/proyecto-pizzeria/src/test/java/com/parula/pizzeria/ProyectoPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parula.pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
